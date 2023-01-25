919ddf85a6b3a1c5b2a5c2f5dcb66b53ccc4098f
nina174 <122985208+nina174@users.noreply.github.com>
2023-01-25 17:10:21 +0100

    Create open_raw.py
    loops through participants, as well as data from different EEG-systems, and conditions

--- open_raw.py
+++ open_raw.py
@@ -1,59 +1,81 @@
 # -*- coding: utf-8 -*-
 """
-Created on Thu Jul  7 09:11:38 2022
+Created on Wed Jan 25 15:41:08 2023
 
 @author: ehrhardtn
 """
 
-import os
-from os.path import isfile
+from os import chdir, listdir
+from os.path import isfile, join
 import numpy as np
 import mne
-import glob
-from mne.preprocessing import (ICA)
 
 ## Import data ##############################################################
 
-raw_folder = ('Y:\\01_Studien\\29_TRONA\\Daten\\Nass\\')
-results_folder = ('Y:\\01_Studien\\29_TRONA\\Analysen_und_Ergebnisse\\Nass\\')
-os.chdir(results_folder)
-#condition = ["TOENE"]
+data_dir = ("Y:\\01_Studien\\29_TRONA\\Daten\\")
+out_dir = ("Y:\\01_Studien\\29_TRONA\\Analysen_und_Ergebnisse\\")
 
-if isfile('preprocessing_150.npy'):
-    table = np.load('preprocessing_150.npy')
-else:
-    table = np.array([['ID', 'no. rejected channel', 'no. rejected components', '% rejected trials']])
+system = ["Nass", "Trocken Artefact Corrected"]
+condition = ["TOENE", "REEG"]
+preposts = ["1", "2"]
 
-for sub in range(29,41): # (1,41) to loop through participants 1 to 40
-
-
-    if isfile(glob.glob(os.path.join(raw_folder, f'{sub}_NA_TOENE*.vhdr'))[0]):
-        raw_file = glob.glob(os.path.join(raw_folder, f'{sub}_NA_TOENE*.vhdr'))[0]
-        eeg_data = mne.io.read_raw_brainvision(raw_file, preload=True)
-        eeg_data.set_channel_types(mapping = {'vEOG': 'eog', 'HEOG': 'eog', 'VEOG2': 'eog'}) # für nass-EEG
-        #print(raw.info)
-        
-        ## Reference data to average of mastoids
-        eeg_data.set_eeg_reference(ref_channels=['M1', 'M2'], ch_type='eeg')
-        
-        if isfile(f'{sub}_NA_badch.txt'):
-            with open(f'{sub}_NA_badch.txt', 'r') as f:
-                bad_ch = [line.rstrip('\n') for line in f]
-        
-            eeg_data.info['bads'] = bad_ch
-            #print(bad_ch)
+for sys in system:
+    
+    sys_dir = join(data_dir, f"{sys}")
+    chdir(join(out_dir, f"{sys}"))
+    
+    for cond in condition:
+             
+        if isfile("preprocessing_150_f{cond}.npy"):
+            table = np.load("preprocessing_150_f{cond}.npy")
+        else:
+            table = np.array([['ID', 'no. rejected channel', 'no. rejected components', '% rejected trials']])
             
-        else: 
-            eeg_data.plot(duration=10, n_channels=len(eeg_data.ch_names), scalings=25e-6) #scale=50uV, all channels, block=True pauses script while plot is open
+            filenames = listdir(sys_dir)
+            
+            for filename in filenames:
+                if filename[-5:] != ".vhdr":
+                    continue
+                
+                eeg_data = mne.io.read_raw_brainvision(filename, preload=True)
+                
+                if "Nass" in sys:
+                    # Set channel types
+                    eeg_data.set_channel_types(mapping = {'vEOG': 'eog', 'HEOG': 'eog', 'VEOG2': 'eog'})
+                    
+                    ## Reference data to average of mastoids
+                    eeg_data.set_eeg_reference(ref_channels=['M1', 'M2'], ch_type='eeg')
+                    
+                    ## Set the montage
+                    eeg_data.set_montage('standard_1005')
+                
+                else: 
+                    # Set channel types
+                    eeg_data.set_channel_types(mapping = {'BIP1': 'eog', 'BIP2': 'eog'}) # BIP1 = horizontal, BIP2 = vertical (left) 
+                    
+                    # Reference data to average of mastoids
+                    eeg_data.set_eeg_reference(ref_channels=['3LD', '3RD'], ch_type='eeg')
+                    
+                    ## Set the montage
+                    digmon = mne.channels.read_dig_fif("Y:\\01_Studien\\29_TRONA\\Allgemeines\\Info_Trocken_EEG\\montage_ANTWaveguard.fif")  
+                    eeg_data.set_montage(digmon)
+                    
+                sub = filename.partition("_")
+                sub = sub[0]
+                    
+                if isfile(f'{sub}_NA_badch.txt'):
+                    with open(f'{sub}_NA_badch.txt', 'r') as f:
+                        bad_ch = [line.rstrip('\n') for line in f]
+        
+                    eeg_data.info['bads'] = bad_ch
+                    
+                else:
+                    eeg_data.plot(duration=10, n_channels=len(eeg_data.ch_names), scalings=25e-6) #scale=50uV, all channels, block=True pauses script while plot is open
                
-            bad_ch = eeg_data.info['bads']
+                    bad_ch = eeg_data.info['bads']
             
-            with open(f'{sub}_NA_badch.txt', 'w') as f:
-                for c in bad_ch:
-                    f.write(str(c) + '\n')
+                    with open(f'{sub}_NA_badch.txt', 'w') as f:
+                        for c in bad_ch:
+                            f.write(str(c) + '\n')
+
         
-        
-        
-        
-    
-    
