bd3fe9b5cb065fe3869df9dd4b4473c4ff3305a8
nina174 <122985208+nina174@users.noreply.github.com>
2023-01-31 15:06:34 +0100

    Update: adjust badch-file for Trocken-EEG

--- open_raw.py
+++ open_raw.py
@@ -6,16 +6,20 @@ Created on Wed Jan 25 15:41:08 2023
 """
 
 from os import chdir, listdir
-from os.path import isfile, join
+from os.path import join
 import mne
+from glob import glob
 
 ## Import data ##############################################################
 
 data_dir = ("Y:\\01_Studien\\29_TRONA\\Daten\\")
 out_dir = ("Y:\\01_Studien\\29_TRONA\\Analysen_und_Ergebnisse\\")
 
-system = ["Nass", "Trocken Artefact Corrected"]
-condition = ["TOENE", "REEG1", "REEG2"]
+#system = ["Nass", "Trocken Artefact Corrected"]
+#condition = ["TOENE", "REEG1", "REEG2"]
+
+system = ["Trocken Artefact Corrected"]
+condition = ["TOENE"]
 
 for sys in system:
     
@@ -52,24 +56,31 @@ for sys in system:
                 eeg_data.set_eeg_reference(ref_channels=['3LD', '3RD'], ch_type='eeg')
                 
                 ## Set the montage
-                digmon = mne.channels.read_dig_fif("Y:\\01_Studien\\29_TRONA\\Allgemeines\\Info_Trocken_EEG\\montage_ANTWaveguard.fif")  
+                digmon = mne.channels.read_dig_fif("Y:\\01_Studien\\29_TRONA\\Allgemeines\\Info_Trocken_EEG\\montage_ANTWaveguard.fif")
+               
+                with open("Y:\\01_Studien\\29_TRONA\\Allgemeines\\Info_Trocken_EEG\\ANTWaveguard_pos_chnames.csv", "r") as fp:
+                    ch_names = [line.rstrip('\n') for line in fp]
+                    
+                digmon.ch_names = ch_names                    
+                
                 eeg_data.set_montage(digmon)
                 
             sub = filename.partition("_")
             sub = sub[0]
-                            
-            if isfile(join(out_dir, f"{sys}", f'{sub}_NA_badch.txt')):
-                with open(join(out_dir, f"{sys}", f'{sub}_NA_badch.txt'), 'r') as f:
+            
+            if glob(join(out_dir, f"{sys}", f"{sub}*badch.txt")) != []:
+                ch_file = glob(join(out_dir, f"{sys}", f"{sub}*badch.txt"))[0]
+                with open(ch_file, 'r') as f:
                     bad_ch = [line.rstrip('\n') for line in f]
-    
-                eeg_data.info['bads'] = bad_ch
-                
-            else:
-                eeg_data.plot(duration=10, n_channels=len(eeg_data.ch_names), scalings=25e-6) #scale=50uV, all channels, block=True pauses script while plot is open
-           
-                bad_ch = eeg_data.info['bads']
         
-                with open(join(out_dir, f"{sys}", f'{sub}_NA_badch.txt'), 'w') as f:
+                eeg_data.info['bads'] = bad_ch
+                    
+            else:
+                eeg_data.plot(duration=10, n_channels=len(eeg_data.ch_names), scalings=25e-6, block = True) #scale=50uV, all channels, block=True pauses script while plot is open
+       
+                bad_ch = eeg_data.info['bads']
+    
+                with open(join(out_dir, f"{sys}", f'{sub}_badch.txt'), 'w') as f:
                     for c in bad_ch:
                         f.write(str(c) + '\n')
                         
